a26701a5a633e0965676bb799024dfd9e76f0d2d
ryota niinomi <ryotakosu@gmail.com>
2014-06-08 16:02:23 +0900

    fix

--- app.js
+++ app.js
@@ -58,6 +58,14 @@ if (app.get('env') === 'development') {
     });
 }
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
